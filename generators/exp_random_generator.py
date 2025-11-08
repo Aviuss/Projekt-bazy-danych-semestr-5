bc92a2026823d2c5a51b8af131a4bf198eafe484
Teodor <24278460+Teodor11@users.noreply.github.com>
2025-11-08 17:15:36 +0100

    Added printing result in random generator

--- generators/exp_random_generator.py
+++ generators/exp_random_generator.py
@@ -1,8 +1,8 @@
 import math
 import random
 
-class Exp_random_generator:
-    def __init__(self, shard_count, dimensions, lambda_value):
+class ExpRandomGenerator:
+    def __init__(self, shard_count, dimensions = 24, lambda_value = 1):
         self.list_of_load_vectors = [None] * shard_count
         self.shard_count = shard_count
         self.lambda_value = lambda_value
@@ -14,3 +14,24 @@ class Exp_random_generator:
             for j in range(self.dimensions):
                 self.list_of_load_vectors[i][j] = random.expovariate(self.lambda_value)
         return self.list_of_load_vectors
+
+    def print_result(self, filename=None):
+        result = ""
+        for i in range(len(self.list_of_load_vectors)):
+            for j in range(len(self.list_of_load_vectors[i])):
+                result += str(self.list_of_load_vectors[i][j])
+                
+                if j < len(self.list_of_load_vectors[i]) - 1:
+                    result += " " 
+        
+            result += "\n"
+        
+        if (filename):
+            file = open(filename, "w")
+            file.write(result)
+            file.close()
+            print("Result saved to:", filename)
+        else:
+            print(result)
+        
+        return
