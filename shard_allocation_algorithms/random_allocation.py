add52197e5c97ad53d746a64fb550f19b48b5996
Kamil <kamil.suwiczak@wp.pl>
2025-11-08 08:36:19 +0100

    added showing data about nodes to rand_alloc

--- shard_allocation_algorithms/random_allocation.py
+++ shard_allocation_algorithms/random_allocation.py
@@ -1,4 +1,6 @@
 import random
+from utils.node import Node
+import numpy as np
 
 class Random_allocation:
     def __init__(self, list_of_nodes):
@@ -8,4 +10,16 @@ class Random_allocation:
         for load_vector in list_of_load_vectors:
             random_uniform_choice = random.randint(0, len(self.list_of_nodes) - 1)
             self.list_of_nodes[random_uniform_choice].add_load_vector(load_vector)
-        return 
+        return
+     
+    def data_of_allocated_vectors(self):
+        data = []
+        for index, node in enumerate(self.list_of_nodes):
+            node_data = {
+                "node": index,
+                "load_vector_count": len(node.list_of_load_vectors),
+                "average": np.mean(node.list_of_load_vectors),
+                "sum": np.sum(node.list_of_load_vectors)
+            }
+            data.append(node_data)
+        return data
