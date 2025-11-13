b75ea3351b43974b4060a7a313e310d2f277e8a7
Aviuss <konradbb09@gmail.com>
2025-11-13 10:43:09 +0100

    create template

--- shard_allocation_algorithms/Multidimentional_Multiway_Number_Partitioning.py
+++ shard_allocation_algorithms/Multidimentional_Multiway_Number_Partitioning.py
@@ -1,8 +1,21 @@
 import random
+import numpy as np
 
 class Multidimentional_Multiway_Number_Partitioning:
     def __init__(self, list_of_nodes):
         self.list_of_nodes = list_of_nodes
 
     def allocate(self, list_of_load_vectors):
-        return 
+        return
+
+    def data_of_allocated_vectors(self):
+        data = []
+        for index, node in enumerate(self.list_of_nodes):
+            node_data = {
+                "node": index,
+                "load_vector_count": len(node.list_of_load_vectors),
+                "average": np.mean(node.list_of_load_vectors),
+                "sum": np.sum(node.list_of_load_vectors)
+            }
+            data.append(node_data)
+        return data
--- tasks/task_1/main.py
+++ tasks/task_1/main.py
@@ -4,6 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(
 from generators.exp_random_generator import ExpRandomGenerator
 from utils.node import Node
 from shard_allocation_algorithms.random_allocation import Random_allocation
+from shard_allocation_algorithms.Multidimentional_Multiway_Number_Partitioning import Multidimentional_Multiway_Number_Partitioning
 import math
 
 if __name__ == "__main__":
@@ -17,11 +18,22 @@ if __name__ == "__main__":
     ).generate()
 
     node_count = round(SHARD_COUNT/AVERAGE_SHARDS_PER_NODE)
-    list_of_nodes = [Node() for _ in range(node_count)]
+    list_of_nodes_for_random_allocation = [Node() for _ in range(node_count)]
+    list_of_nodes_for_multiway_partitioning = [Node() for _ in range(node_count)]
 
-    random_allocated = Random_allocation(list_of_nodes)
+    random_allocated = Random_allocation(list_of_nodes_for_random_allocation)
     random_allocated.allocate(list_of_load_vectors)
     data_random_allocated = random_allocated.data_of_allocated_vectors()
     
+    multiway_number_partitioning = Multidimentional_Multiway_Number_Partitioning(list_of_nodes_for_multiway_partitioning)
+    multiway_number_partitioning.allocate(list_of_load_vectors)
+    data_multiway_allocated = multiway_number_partitioning.data_of_allocated_vectors()
+
+    print("\n## Data random allocated")
     for _ in data_random_allocated:
         print(_)
+    
+    print("\n## Data multiway allocated")
+    for _ in data_multiway_allocated:
+        print(_)
+    print("\n")
