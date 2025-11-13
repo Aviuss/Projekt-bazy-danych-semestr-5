f373b71fca7de5af3cb23cfa412d71c733bb21e2
Aviuss <konradbb09@gmail.com>
2025-11-13 10:29:00 +0100

    create file and delete old comments

---
+++ shard_allocation_algorithms/Multidimentional_Multiway_Number_Partitioning.py
@@ -0,0 +1,8 @@
+import random
+
+class Multidimentional_Multiway_Number_Partitioning:
+    def __init__(self, list_of_nodes):
+        self.list_of_nodes = list_of_nodes
+
+    def allocate(self, list_of_load_vectors):
+        return 
