e788bc9bfec35cf899e024e16ea63b808eff675b
Aviuss <konradbb09@gmail.com>
2025-11-13 13:29:24 +0100

    implemented multiway number allocation algorithm

--- shard_allocation_algorithms/Multidimentional_Multiway_Number_Partitioning.py
+++ shard_allocation_algorithms/Multidimentional_Multiway_Number_Partitioning.py
@@ -6,7 +6,65 @@ class Multidimentional_Multiway_Number_Partitioning:
         self.list_of_nodes = list_of_nodes
 
     def allocate(self, list_of_load_vectors):
-        return
+        if len(list_of_load_vectors) == 0:
+            return
+
+        list_of_load_vectors.sort(
+            key = lambda listx: max(
+                abs(max(listx)),
+                abs(min(listx))
+            ), # sort by maximum norm
+            reverse=True
+        ) # sort from largest to smallest
+        
+        dim = len(list_of_load_vectors[0])
+
+        for examined_vector in list_of_load_vectors:
+            nodes_len = len(self.list_of_nodes)
+            vector_sum_with_examined_vector = [
+                self.add_vectors(
+                    self.list_of_nodes[i].vector_sum_of_load_vectors(if_null_default_dimention=dim),
+                    examined_vector
+                )
+                for i in range(nodes_len)
+            ]
+            maximum_norm_for_vectsum_w_examvect = [
+                max(
+                    abs(max(vector_sum_with_examined_vector[i])),
+                    abs(min(vector_sum_with_examined_vector[i]))
+                )
+                for i in range(nodes_len)
+            ]
+
+            min_maxnorm = {
+                "min_value": None,
+                "list_of_node_indexes": []
+            }
+
+            for i in range(nodes_len):
+                isSet = min_maxnorm["min_value"] != None
+                isEqual = isSet and maximum_norm_for_vectsum_w_examvect[i] == min_maxnorm["min_value"]
+                isSmaller = isSet and maximum_norm_for_vectsum_w_examvect[i] < min_maxnorm["min_value"]
+                
+                if  isSmaller or not isSet:
+                    min_maxnorm["min_value"] = maximum_norm_for_vectsum_w_examvect[i]
+                    min_maxnorm["list_of_node_indexes"] = [i]
+                elif isEqual:
+                    min_maxnorm["list_of_node_indexes"].append(i)
+            
+            if min_maxnorm["min_value"] == None:
+                raise Exception("Sth went horribly wrong, probably nodes_len is not set")
+
+            selected_node_idx = None
+            if len(min_maxnorm["list_of_node_indexes"]) == 1:
+                selected_node_idx = min_maxnorm["list_of_node_indexes"][0]
+            else:
+                random_uniform_choice = random.randint(0, len(min_maxnorm["list_of_node_indexes"]) - 1)
+                selected_node_idx = min_maxnorm["list_of_node_indexes"][random_uniform_choice]
+
+            self.list_of_nodes[selected_node_idx].add_load_vector(examined_vector)
+        
+
 
     def data_of_allocated_vectors(self):
         data = []
@@ -19,3 +77,15 @@ class Multidimentional_Multiway_Number_Partitioning:
             }
             data.append(node_data)
         return data
+
+    def add_vectors(self, a, b):
+        K = len(a)
+        if K != len(b):
+            raise Exception("Can't add vectors with different dimentions")
+        
+        sum_vector = [0 for _ in range(K)]
+        for k in range(K):
+            sum_vector[k] += a[k]
+            sum_vector[k] += b[k]
+
+        return sum_vector
--- utils/node.py
+++ utils/node.py
@@ -4,3 +4,20 @@ class Node:
 
     def add_load_vector(self, load_vector):
         self.list_of_load_vectors.append(load_vector)
+
+    def vector_sum_of_load_vectors(self, if_null_default_dimention = None):
+        if len(self.list_of_load_vectors) == 0:
+            if if_null_default_dimention == None:
+                return []
+            else:
+                return [0 for _ in range(if_null_default_dimention)]
+
+        K = len(self.list_of_load_vectors[0])
+        sum_vector = [0 for _ in range(K)]
+        for vector in self.list_of_load_vectors:
+            for k in range(K):
+                sum_vector[k] += vector[k]
+    
+        return sum_vector
+
+        
