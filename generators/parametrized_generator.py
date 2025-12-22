108de6ee669fac8d76708dbc5e28d92bc24b367a
Kamil <kamil.suwiczak@wp.pl>
2025-12-22 12:22:45 +0100

    changed generator

--- generators/parametrized_generator.py
+++ generators/parametrized_generator.py
@@ -7,6 +7,7 @@ import math
 import random
 import numpy as np
 import pandas as pd
+from scipy.optimize import differential_evolution
 
 
 class ParametrizedGenerator(InputOutput):
@@ -21,71 +22,99 @@ class ParametrizedGenerator(InputOutput):
         self.KI: float = KI
         self.CN: int = CN
         self.dimensions: int = dimensions
+
+
+    def calculate_KO(self, group_avg_amplitude: List[float], n_points=1000) -> List[float]:
+        target_KO_Matrix = np.full((self.N, self.N), self.KO)
+        np.fill_diagonal(target_KO_Matrix, 1)
         
+        def calculate_load_base_group(offset, amplitude):
+            t = np.linspace(0, 2*np.pi * self.CN, n_points)
+            sinusoid = amplitude * np.sin(t + offset)
+            return sinusoid
+
+
+        def cost(offsets: List[float]) -> int:
+            vectors = []
+            for i in range(len(group_avg_amplitude)):
+                vectors.append(calculate_load_base_group(offsets[i], group_avg_amplitude[i]))
+            vectors = np.array(vectors)
+            corr_matrix = np.corrcoef(vectors)
+            c = np.sum(np.abs(target_KO_Matrix - corr_matrix)**10)
+                
+            return c
     
+        bounds = [(0, 2*np.pi)] * self.N
+
+        result = differential_evolution(cost, bounds, strategy='currenttobest1bin', maxiter=1000, popsize=20, recombination=0.3, tol=1e-6, mutation=0.3)   
+
+        best_offsets = result.x
+        vectors_opt = np.array([calculate_load_base_group(o, a) for o, a in zip(best_offsets, group_avg_amplitude)])
+        corr_matrix_opt = np.corrcoef(vectors_opt)
+
+        print("KO debugs:")
+        print("Best offsets:", best_offsets)
+        print("Minimum cost:", result.fun)
+        print("Target KO:", self.KO)
+        print("Optimized correlation matrix:\n", np.round(corr_matrix_opt, 2))
+        print("Average correlation", (np.sum(corr_matrix_opt) - self.N)/(self.N*self.N - self.N))
+
+        return best_offsets
+
+
+    def assign_to_groups(self) -> list[int]:
+        base_size = self.S // self.K
+        sizes = [base_size] * self.K
+
+        rest = self.S % self.K
+        for i in range(rest):
+            sizes[i] += 1
+
+        current_sd = np.std(sizes)
+        iteration = 0
+        max_iterations = 1000
+
+        while abs(current_sd - self.D) > 0.1 and iteration < max_iterations and self.D != 0:
+            iteration += 1
+
+            if current_sd < self.D:
+                sizes = sorted(sizes)
+                for i in range(self.K):
+                    if sizes[i] > 0:
+                        sizes[i] -= 1
+                        break
+                sizes[-1] += 1
+
+            elif current_sd > self.D:
+                sizes = sorted(sizes, reverse=True)
+                for i in range(self.K):
+                    if sizes[i] > 0:
+                        sizes[i] -= 1
+                        break
+                sizes[-1] += 1
+
+            current_sd = np.std(sizes)
+
+        current_group = 0
+        result = []
+        for size in sizes:
+            result.extend([current_group] * size)
+            current_group += 1
+
+        print("Final standard deviation:", current_sd)
+        return result
+
     def generate(self) -> pd.DataFrame:
 
         shards_groups: List[Dict[str,int | float]] = [{"shard_index": [], "amplitude": 0.0, "offset_x": 0.0} for _ in range(self.K)]
         vectors_amplitude = [0.0] * self.S
 
-        def assign_to_groups() -> list[int]:
-            base_size = self.S // self.K
-            sizes = [base_size] * self.K
-
-            rest = self.S % self.K
-            for i in range(rest):
-                sizes[i] += 1
-
-            current_sd = np.std(sizes)
-            iteration = 0
-            max_iterations = 1000
-
-            while abs(current_sd - self.D) > 0.1 and iteration < max_iterations and self.D != 0:
-                iteration += 1
-
-                if current_sd < self.D:
-                    sizes = sorted(sizes)
-                    for i in range(self.K):
-                        if sizes[i] > 0:
-                            sizes[i] -= 1
-                            break
-                    sizes[-1] += 1
-
-                elif current_sd > self.D:
-                    sizes = sorted(sizes, reverse=True)
-                    for i in range(self.K):
-                        if sizes[i] > 0:
-                            sizes[i] -= 1
-                            break
-                    sizes[-1] += 1
-
-                current_sd = np.std(sizes)
-
-            current_group = 0
-            result = []
-            for size in sizes:
-                result.extend([current_group] * size)
-                current_group += 1
-
-            print("Final standard deviation:", current_sd)
-            return result
-
-        groups_assignment = assign_to_groups()
+        groups_assignment = self.assign_to_groups()
         print("Group assignment:", groups_assignment)
         for i in range(self.S):
             group_index = groups_assignment[i]
             shards_groups[group_index]["shard_index"].append(i)
 
-        def calculate_KO(offset1, offset2, a1, a2, n_points=1000) -> float:
-            t = np.linspace(0, 2*np.pi * self.CN, n_points)
-
-            sinusoid1 = a1 * np.sin(t + offset1)
-            sinusoid2 = a2 * np.sin(t + offset2)
-            
-            corr_matrix = np.corrcoef(sinusoid1, sinusoid2)
-            
-            correlation_value = corr_matrix[0, 1]
-            return correlation_value
         
         for i in range(self.K):
             amplitude = random.uniform(0, 1)
@@ -97,22 +126,24 @@ class ParametrizedGenerator(InputOutput):
             for shard_index in shards_groups[i]["shard_index"]:
                 vectors_amplitude[shard_index] = np.random.lognormal(mean=amplitude, sigma=math.sqrt(var))
         
-        curr_KO = 0.0
-        offset1 = shards_groups[0]["offset_x"]
-        amplitude1 = shards_groups[0]["amplitude"]
-        offset2 = shards_groups[1]["offset_x"]
-        amplitude2 = shards_groups[1]["amplitude"]
-        while (abs(self.KO - curr_KO)>0.001):
-            offset1 += 0.01
-            offset1 = offset1 % (2*math.pi) 
-            curr_KO = calculate_KO(offset1, offset2, amplitude1, amplitude2)
-            shards_groups[0]["offset_x"] = offset1
+        # curr_KO = 0.0
+        # offset1 = shards_groups[0]["offset_x"]
+        # amplitude1 = shards_groups[0]["amplitude"]
+        # offset2 = shards_groups[1]["offset_x"]
+        # amplitude2 = shards_groups[1]["amplitude"]
+
+        # offsets = [shards_groups[i]["offset_x"] for i in range(self.K)]
+        amplitudes = [shards_groups[i]["amplitude"] for i in range(self.K)]
+        offsets = self.calculate_KO(amplitudes)
+        
         
         for i in range(self.K):
             for shard_index in shards_groups[i]["shard_index"]:
                 load_vector:List[float] = [None] * self.dimensions
                 amplitude = vectors_amplitude[shard_index]
-                offset_x = shards_groups[i]["offset_x"]
+                # offset_x = shards_groups[i]["offset_x"]
+                offset_x = offsets[i]
+
                 for d in range(self.dimensions):
                     x = (2 * math.pi * self.CN * d) / self.dimensions
                     load_vector[d] = amplitude * math.sin(x + offset_x)+ amplitude
@@ -125,3 +156,8 @@ class ParametrizedGenerator(InputOutput):
             print(f"Shard {i}: {vector}")
     
 
+if __name__ == "__main__":
+    generator = ParametrizedGenerator(S=10, N=5, R=0.5, KO=0.7, CN=3, K=2, KI=1, dimensions=24, D=1.0)
+    df = generator.generate()
+    generator.print_results()
+    print(df)
